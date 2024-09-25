f0b8318f9002dadbf68cc3496092db9adb46cf98
Lukas Boettcher <1340215+lukasboettcher@users.noreply.github.com>
2024-09-26 01:42:05 +0200

    improve dev env

--- examples/debug.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm -it --network host -v `pwd`/grafana.ini:/etc/grafana/grafana.ini grafana/grafana
-go run . -label job -upstream http://demo.do.prometheus.io:9090 \
-    -insecure-listen-address 127.0.0.1:8080 \
-    -oidc-client-id XXX \
-    -oidc-issuer https://login.microsoftonline.com/XXX/v2.0 \
-    -oidc-config examples/tenant-config.yaml
